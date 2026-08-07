67d65d76241dd77bd4cf90a7963b587667b8a03f
Jack Maders <jackwmaders@gmail.com>
2026-08-07 11:11:28 +0100

    fix(build): ⚡ export force-dynamic segment config for D1 edge route handlers to allow build-time SSG

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,2 @@
+export const dynamic = "force-dynamic";
 export { HomePage as default } from "@/_pages/home";
--- app/vods/page.tsx
+++ app/vods/page.tsx
@@ -1,1 +1,2 @@
+export const dynamic = "force-dynamic";
 export { VodsPage as default } from "@/_pages/vods";
