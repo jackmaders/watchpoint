0e389f79ed5b887165a69a42c842501348830af7
Jack Maders <jackwmaders@gmail.com>
2026-08-07 11:30:55 +0100

    refactor: update getCloudflareContext invocation to support asynchronous execution

--- src/shared/db/client.ts
+++ src/shared/db/client.ts
@@ -2,7 +2,7 @@ import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { PrismaD1 } from "@prisma/adapter-d1";
 import { PrismaClient } from "../../../generated/prisma/client";
 
-const { env } = getCloudflareContext();
+const { env } = await getCloudflareContext({ async: true });
 
 const adapter = new PrismaD1(env.DB);
 
