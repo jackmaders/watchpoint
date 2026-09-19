df63359eeb0a025918fb24c9c21e99c79be66048
Jack Maders <jackwmaders@gmail.com>
2026-09-19 16:06:33 +0100

    temp: temp commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
+import { getPosts } from "#/db/queries";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
+	const posts = getPosts();
+
 	return (
 		<div className="p-8">
 			<h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
 			<p className="mt-4 text-lg">
 				Edit <code>src/routes/index.tsx</code> to get started.
 			</p>
+			<>{posts}</>
 		</div>
 	);
 }
