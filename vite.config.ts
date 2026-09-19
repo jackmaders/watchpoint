3650dbeaa3ad81f386c9511139eae2cf4842dc5d
Jack Maders <42032268+jackmaders@users.noreply.github.com>
2026-09-19 11:49:28 +0100

    feat: ☁️ add initial wranger config for cloudflare (#2)
    * add cloudflare
    
    * update file change check

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
+	resolve: { tsconfigPaths: true },
+	plugins: [
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		devtools(),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+	],
+});
 
-export default config
+export default config;
