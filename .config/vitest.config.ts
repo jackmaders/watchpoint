b4e1f4ff3a83d1d46d9ad68f9e459bb4dffe1a87
Jack Maders <42032268+jackmaders@users.noreply.github.com>
2026-09-20 12:19:23 +0100

    chore: improve build and unit tests (#15)
    * chore: improve build and unit tests
    
    * test: add tests

--- .config/vitest.config.ts
+++ .config/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	resolve: { tsconfigPaths: true },
 	test: {
+		mockReset: true,
 		environment: "happy-dom",
 		globals: true,
 		include: ["src/**/__tests__/*.spec.{ts,tsx}"],
