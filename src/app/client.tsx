c6732f1a73094fb877faacf19c0c5d9125eb6b0a
Jack Maders <jackwmaders@gmail.com>
2026-09-23 14:40:50 +0100

    chore: remove redundant typeof window check in client entry

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -3,21 +3,18 @@ import posthog from "posthog-js";
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-if (typeof window !== "undefined") {
-	const posthogKey = import.meta.env.VITE_POSTHOG_KEY;
-	const posthogHost =
-		import.meta.env.VITE_POSTHOG_HOST || "https://us.i.posthog.com";
+const posthogKey = import.meta.env.VITE_POSTHOG_KEY;
+const posthogHost =
+	import.meta.env.VITE_POSTHOG_HOST || "https://us.i.posthog.com";
 
-	if (posthogKey) {
-		posthog.init(posthogKey, {
-			// biome-ignore lint/style/useNamingConvention: PostHog SDK option
-			api_host: posthogHost,
-			// biome-ignore lint/style/useNamingConvention: PostHog SDK option
-			capture_exceptions: true,
-			// biome-ignore lint/style/useNamingConvention: PostHog SDK option
-			person_profiles: "identified_only",
-		});
-	}
+if (posthogKey) {
+	posthog.init(posthogKey, {
+		// biome-ignore-start lint/style/useNamingConvention: PostHog SDK option
+		api_host: posthogHost,
+		capture_exceptions: true,
+		person_profiles: "identified_only",
+		// biome-ignore-end lint/style/useNamingConvention: PostHog SDK option
+	});
 }
 
 startTransition(() => {
