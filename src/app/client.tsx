2e0cff1aa2fc48cbed6615ad32ab0269f62d144f
Jack Maders <jackwmaders@gmail.com>
2026-09-23 14:44:50 +0100

    chore: cleanup posthog

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,21 +1,10 @@
+// biome-ignore-all lint/style/useNamingConvention: PostHog SDK options
+
 import { StartClient } from "@tanstack/react-start/client";
-import posthog from "posthog-js";
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-const posthogKey = import.meta.env.VITE_POSTHOG_KEY;
-const posthogHost =
-	import.meta.env.VITE_POSTHOG_HOST || "https://us.i.posthog.com";
-
-if (posthogKey) {
-	posthog.init(posthogKey, {
-		// biome-ignore-start lint/style/useNamingConvention: PostHog SDK option
-		api_host: posthogHost,
-		capture_exceptions: true,
-		person_profiles: "identified_only",
-		// biome-ignore-end lint/style/useNamingConvention: PostHog SDK option
-	});
-}
+import "./telemetry/posthog.client";
 
 startTransition(() => {
 	hydrateRoot(
---
+++ src/app/telemetry/posthog.client.ts
@@ -0,0 +1,10 @@
+import "@tanstack/react-start/client-only";
+
+import posthog from "posthog-js";
+
+if (import.meta.env.VITE_POSTHOG_KEY) {
+	posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+		api_host: import.meta.env.VITE_POSTHOG_HOST,
+		capture_exceptions: true,
+	});
+}
