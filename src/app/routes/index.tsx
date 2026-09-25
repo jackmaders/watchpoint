4621af3e64cefd8a0b8e5cad331d05a165c53e3d
Jack Maders <jackwmaders@gmail.com>
2026-09-25 11:00:33 +0100

    chore: remove unnecessary import

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import type { LessonSummary } from "@/entities/lesson";
 import { postListQueryOptions } from "@/entities/post";
 import { publishedVodListQueryOptions } from "@/entities/vod";
 import { HomePage } from "@/pages/home";
-
-interface HomeRouteData {
-	lessonSummaryPreview: LessonSummary | null;
-}
 
 export const Route = createFileRoute("/")({
 	loader: async ({ context }) => {
@@ -14,7 +9,6 @@ export const Route = createFileRoute("/")({
 			context.queryClient.ensureQueryData(postListQueryOptions),
 			context.queryClient.ensureQueryData(publishedVodListQueryOptions),
 		]);
-		return { lessonSummaryPreview: null } satisfies HomeRouteData;
 	},
 	component: HomePage,
 });
